8c8656ec9c00b909b80d2e8f9ad811f0f43d0bda
Druwing <143631126+Druwing@users.noreply.github.com>
2025-06-01 21:04:26 -0300

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -7,11 +7,9 @@ load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 
-MONGO_URI = os.getenv("MONGODB_ATLAS_URI")
-
 
 try:
-    client = MongoClient(MONGO_URI)
+    client = MongoClient( os.getenv("MONGODB_ATLAS_URI"))
     db = client.get_database()
     logging.info("✅ Conectado ao MongoDB com sucesso!")
 except Exception as e:
