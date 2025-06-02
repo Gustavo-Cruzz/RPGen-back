e181271e225e361250593905ee9e166cf4578b82
Druwing <juan.santos@aln.senaicimatec.edu.br>
2025-06-02 10:07:44 -0300

    o

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
-    CORS_ORIGINS: List[str] = ["http://localhost:3000"]
+    CORS_ORIGINS: List[str] = ["*"]
     
     class Config:
         env_file = ".env"
